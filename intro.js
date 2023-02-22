f9f3670f71dae3763462ae90db8c2ac5ecda7da6
Purushotam Sharma <107574991+PurushotamSharma@users.noreply.github.com>
2023-02-22 22:58:54 +0530

    Purushotam

---
+++ intro.js
@@ -0,0 +1,7 @@
+// // console.log can print something on console
+// console.log("Hello World");
+let String1 = "Purushotam";
+let String2 = "Sharma";
+
+let fullName = String1 + " " + String2;
+console.log(fullName);
